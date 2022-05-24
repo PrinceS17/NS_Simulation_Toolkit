5b7387de7f10e0104d853299277ea3fe4a4cf9ff
Jinhui Song <jinhuis2@illinois.edu>
2022-05-24 00:48:06 -0500

    Notice for recompilation.

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ cd BRITE
 make
 cd ..
 
+# TODO: may need recompile waf on new platform before using it
 git clone https://github.com/PrinceS17/BBR_test.git
 cd BBR_test/ns-3.27
 CXXFLAGS="-Wall" ./waf configure --with-brite=../../BRITE --enable-examples --enable-sudo --visualize
