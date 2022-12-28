3b99a3304c5c918d2620260d8d8b09658ea4851e
Jinhui Song <jinhuis2@golubh4.campuscluster.illinois.edu>
2022-12-28 02:16:08 -0600

    Update submit all

--- script/submit_all.sh
+++ script/submit_all.sh
@@ -6,20 +6,20 @@ SEC_SETTING="--time=4:00:00 --partition=secondary"
 
 TRAIN_DIR="train_tailored"
 TEST_DIR="test_basic"
-POST_CUT='cut -f 4 -d " "'
+#POST_CUT='cut -f 4 -d " "'
 
-# 4 x 36 runs
-JOB_TL_S1=$(sbatch ${SEC_SETTING} ${TRAIN_DIR}/left_small.sbatch | ${POST_CUT})
-JOB_TL_S2=$(sbatch --dependency=afterany:${JOB_TL_S1} ${SEC_SETTING} ${TRAIN_DIR}/left_small.sbatch | ${POST_CUT})
-JOB_TR_S1=$(sbatch --dependency=afterany:${JOB_TL_S2} ${SEC_SETTING} ${TRAIN_DIR}/right_small.sbatch | ${POST_CUT})
-JOB_TR_S2=$(sbatch --dependency=afterany:${JOB_TR_S1} ${SEC_SETTING} ${TRAIN_DIR}/right_small.sbatch | ${POST_CUT})
+if [[ $1 == "train" ]]; then
+    # 4 x 36 runs
+    JOB_TL_S1=$(sbatch ${SEC_SETTING} ${TRAIN_DIR}/left_small.sbatch | cut -f 4 -d " ")
+    JOB_TL_S2=$(sbatch --dependency=afterany:${JOB_TL_S1} ${SEC_SETTING} ${TRAIN_DIR}/left_small.sbatch | cut -f 4 -d " ")
+    JOB_TR_S1=$(sbatch --dependency=afterany:${JOB_TL_S2} ${SEC_SETTING} ${TRAIN_DIR}/right_small.sbatch | cut -f 4 -d " ")
+    JOB_TR_S2=$(sbatch --dependency=afterany:${JOB_TR_S1} ${SEC_SETTING} ${TRAIN_DIR}/right_small.sbatch | cut -f 4 -d " ")
 
-
-# test runs
-sbatch ${CSL_SETTING} ${TEST_DIR}/large_flow_L.sbatch
-sbatch ${CSL_SETTING} ${TEST_DIR}/para_btnk_L.sbatch
-sbatch ${CSL_SETTING} ${TEST_DIR}/load_scan_L.sbatch
-sbatch ${CSL_SETTING} ${TEST_DIR}/one_to_n_L.sbatch
-sbatch ${ENG_SETTING} ${TEST_DIR}/path_lag_M.sbatch
-
-
+elif [[ $1 == "test" ]]; then
+    # test runs
+    sbatch ${CSL_SETTING} ${TEST_DIR}/large_flow_L.sbatch
+    sbatch ${CSL_SETTING} ${TEST_DIR}/para_btnk_L.sbatch
+    sbatch ${CSL_SETTING} ${TEST_DIR}/load_scan_L.sbatch
+    sbatch ${CSL_SETTING} ${TEST_DIR}/one_to_n_L.sbatch
+    sbatch ${ENG_SETTING} ${TEST_DIR}/path_lag_M.sbatch
+fi
